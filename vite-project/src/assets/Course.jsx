0407c869799e1f89e341d9ad5d871bf931cdcf3a
Nivash-E <nivashnivash1986@gmail.com>
2026-02-04 19:23:21 +0530

    JAVAAA TO JAVA

--- vite-project/src/assets/Course.jsx
+++ vite-project/src/assets/Course.jsx
@@ -5,7 +5,7 @@ function Course(){
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
-                <li>JavaaaaScript</li>
+                <li>JavaScript</li>
                 <li>ReactJS</li>
                 <li>NodeJS</li>
                 
