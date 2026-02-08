f6f6f56e2c8160b0a1b376e4344de512207feeca
Nivash-E <nivashnivash1986@gmail.com>
2026-02-08 15:52:39 +0530

    image added

--- vite-project/src/Course.jsx
+++ vite-project/src/Course.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function Course(props){
            return(
-        <div className="card">
+        props.name &&<div className="card">
             <img src={props.img} alt="" />
             <h3>{props.name}</h3>
              <p>{props.price}</p>
--- vite-project/src/CourseList.jsx
+++ vite-project/src/CourseList.jsx
@@ -2,34 +2,86 @@ import Course from './Course.jsx';
 import One from './assets/one.jpg';
 import Two from './assets/two.jpg';
 import Three from './assets/three.jpg';
+import murugan1 from './assets/murugan1.jpg';
+import murugan2 from './assets/murugan2.jpg';
+import murugan3 from './assets/murugan3.jpg';
+import ganapathi1 from './assets/ganapathi1.jpg';
+import ganapathi2 from './assets/ganapathi2.jpg';
+import ganapathi3 from './assets/ganapathi3.jpg';
 
 function CourseList(){
     
-        const courses = [
+        const frames = [
             {
                 id:1,
                 name: "Sivan Frame1",
-                price: "$100",
+                price: 100,
                 img: One,
-                rating : 4.5
+                rating : 5.0
             },
             {
                 id:2,
                 name: "Sivan Frame2",
-                price: "$200",
+                price: 200,
                 img: Two,
-                rating : 4.2
+                rating : 4.3
             },
             {  
                 id:3,
                 name: "Sivan Frame3",
-                price: "$300",
+                price: 300,
                 img: Three,
                 rating : 4.8
             },
+            {  
+                id:4,
+                name: "Murugan Frame1",
+                price: 200,
+                img: murugan1,
+                rating : 4.8
+            },
+            {  
+                id:5,
+                name: "Murugan Frame2",
+                price: 300,
+                img: murugan2,
+                rating : 4.5
+            },
+            {  
+                id:6,
+                name: "Murugan Frame3",
+                price: 400,
+                img: murugan3,
+                rating : 5.0
+            },
+            {  
+                id:7,
+                name: "Ganapathi Frame1",
+                price: 250,
+                img: ganapathi1,
+                rating : 5.0
+            },
+            {  
+                id:6,
+                name: "Ganapathi Frame2",
+                price: 350,
+                img: ganapathi2,
+                rating : 4.3
+            },
+            {  
+                id:8,
+                name: "Ganapathi Frame3",
+                price: 400,
+                img: ganapathi3,
+                rating : 4.9
+            },
         ]
 
-        const courseList = courses.map((course) =><Course key={course.id} name={course.name}
+        frames.sort((a,b) => b.rating - a.rating);
+
+        const topPriceFrames = frames.filter(course => course.price >= 300);
+
+        const courseList = topPriceFrames.map((course,index) =><Course key={index} name={course.name}
          price={course.price} img={course.img}
           rating={course.rating}/>)
     return(
